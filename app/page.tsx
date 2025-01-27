ece28d6e570aa7dd2bd96fd53b2dad431d0de17a
Satheshwaran V <satheswaran6677@gmail.com>
2025-01-27 16:52:03 +0530

    Refactor: Rearrange component imports and rendering order in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';
 import DashboardImage from '@/components/DashboardImage';
 import TrustedBrands from '@/components/TrustedBrands';
 import Works from '@/components/Work';
 import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions';
 import WhyUs from '@/components/WhyUs';
-import Footer from '@/components/Footer';
 import IdeaSection from '@/components/IdeaSection';
 import ExploreCourses from '@/components/ExploreCourses';
 import Services from '@/components/Services';
@@ -19,19 +17,15 @@ const Home: NextPage = () => {
         <title>Divine Infotech</title>
         <meta name="description" content="Next-Generation AI Tools for Smarter Business" />
       </Head>
-
       <HeroSection />
       <DashboardImage />
       <TrustedBrands />
       <Works />
-
-
+      <FrequentlyAskedQuestions />
+      <WhyUs />
+      <IdeaSection />
       <ExploreCourses />
       <Services />
-      <WhyUs />
-      <FrequentlyAskedQuestions />
-
-      <IdeaSection />
     </div>
   );
 };
