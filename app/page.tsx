6a581b63769699247e6ee2af2f42f64fa3802e0c
Satheshwaran V <satheswaran6677@gmail.com>
2025-02-13 20:06:03 +0530

    view

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import Head from 'next/head';
 import HeroSection from '@/components/HeroSection';
 import TrustedBrands from '@/components/TrustedBrands';
-import Works from '@/components/Work';
+
 import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions';
 import DashboardImage from '@/components/DashboardImage';
 import WhyUs from '@/components/WhyUs';
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
       <HeroSection />
       <DashboardImage />
       <TrustedBrands />
-      {/* <Works /> */}
+
       <Services />
       <ExploreCourses />
       <WhyUs />
